89bd79620ec1e5ad164682f133e275b8a5f59a3b
JersonCortes <JersonCortes@proton.me>
2024-06-07 20:09:59 -0600

    Command is working, but returns a unexpected lua table

--- init.lua
+++ init.lua
@@ -13,46 +13,50 @@ M.show_containers = function(opts)
 	pickers.new(opts,{
 		finder = finders.new_async_job({
 			command_generator = function ()
-				return {"podman", "images", "--format", "json"}
+				local command = "podman ps"
+				local aux = "--format json | jq -c"
+				local fullCommand = command .. ' ' .. aux
+				return {"sh", "-c", fullCommand}
 			end,
 
-			entry_maker = function (entry)
-				log.debug(entry)
-				local parsed = vim.json.decode(entry)
-				log.debug(parsed)
-				return {
-					value = entry,
-					display = entry.name,
-					ordinal = entry.name,
-				}
-			end
-		}),
+				entry_maker = function (entry)
+					log.debug(entry)
+					local parsed = vim.json.decode(entry)
+					log.debug(parsed)
+					log.debug('Accessing: ', parsed[1].Image)
+					log.debug('Accessing: ', parsed[2].Image)
 
-		sorter = config.generic_sorter(opts),
+					return {
+						value = parsed,
+						display = parsed[1].Image,
+						ordinal = parsed[1].Image,
+					}
+				end
+			}),
 
-		previewer = previewers.new_buffer_previewer({
-			title = "Container Image details",
-			define_preview = function (self, entry)
-				vim.api.nvim_buf_set_lines(
+			sorter = config.generic_sorter(opts),
+
+			previewer = previewers.new_buffer_previewer({
+				title = "Container Image details",
+				define_preview = function (self, entry)
+					vim.api.nvim_buf_set_lines(
 					self.state.bufnr,
 					0,
 					0,
 					true,
 					vim.tbl_flatten({
-					"test",
-					"testing",
-					"```lua",
-					vim.split(vim.inspect(entry.value), "\n"),
-					"```"
+						"```lua",
+						vim.split(vim.inspect(entry.value), "\n"),
+						"```"
 					})
-				)
-			utils.highlighter(self.state.bufnr, 'markdown')
-			end
-		})
+					)
+					utils.highlighter(self.state.bufnr, 'markdown')
+				end
+			})
 
-	}):find()
-end
+		}):find()
+	end
 
-M.show_containers()
+	M.show_containers()
 
-return M
+	return M
