96776c9a3c8e178d362e6100cae05660ac4db113
JersonCortes <JersonCortes@proton.me>
2024-06-14 13:46:39 -0600

    organized folders

--- lua/telescope/_extensions/telescope_podman.lua
+++ lua/telescope/_extensions/telescope_podman.lua
@@ -1,9 +1,8 @@
-local telescope_podman = require('telescope_podman')
+local telescope_podman = require('telescope-podman')
 
 return require('telescope').register_extension({
     exports = {
         show_images = telescope_podman.show_images,
         show_containers = telescope_podman.show_containers,
     },
-    log.debug("test")
 })
